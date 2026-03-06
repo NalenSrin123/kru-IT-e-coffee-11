db06993abff6610c12b2fc4c8a487fe9fa9639cd
phalla <landphalll@gmail.com>
2026-03-06 14:19:50 +0700

    design otp page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import OtpPage from './services/auth/OtpPage'
 
 const App = () => {
   return (
-    <div className='text-amber-400'>App</div>
+    <div>
+    </div>
   )
 }
 
