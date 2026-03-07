4e254cccf7cd962c3ccef2a6aaaa7ac5ffc3dfcd
laomphannya240-ops <laomphannya240@gmail.com>
2026-03-06 22:31:03 -0800

    update

---
+++ src/services/auth/register.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+
+export default function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    alert("Register Successful");
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-300">
+      
+      <div className="w-full max-w-md bg-white p-8 rounded-2xl shadow-lg">
+        
+        <h2 className="text-2xl font-bold text-center mb-6">
+          Create Account
+        </h2>
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+
+          <div>
+            <label className="block text-sm font-medium mb-1">
+              Full Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium mb-1">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium mb-1">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter password"
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              required
+            />
+          </div>
+
+
+
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
+          >
+            Register
+          </button>
+          
+        </form>
+
+        <p className="text-sm text-center mt-4">
+          Already have an account? 
+          <span className="text-blue-500 cursor-pointer ml-1">
+            Login
+          </span>
+        </p>
+
+      </div>
+      
+    </div>
+  );
+}
