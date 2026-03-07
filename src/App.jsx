89b55209984e0610188dc80deffb7503c6590f93
NalenSrin123 <srinnalen@gmail.com>
2026-03-07 12:14:42 +0700

    Show result

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 
-import {BrowserRouter, Routes, Route} from "react-router-dom"
 import React from 'react'
 import Design_UI_get_reset_password_in_email from './services/auth/Design_UI_get_reset_password_in_email'
+import ResetPassword from './services/auth/Comfirm_rest_pw'
+import ForgotPassword from './services/auth/Forgotpassword'
+import GetOtpInEmail from './services/auth/GetOtpInEmail'
+import OtpDesign from './services/auth/OtpPage'
+import Sidebar from './dashboard/features/Sidebar/sidebar'
+import { DashboardOverview } from './dashboard/features/dashboard-overview/DashboardMain'
+import CustomerLists from './dashboard/features/a2-tables-filters-ux/confirmation-modals/Customerlists'
+import HeroSection from './components/public/home/HeroSection'
+import Design_Top_Product from './components/public/home/Design_Top_Product'
 
 function App() {
   return (
     <div>
       <Design_UI_get_reset_password_in_email/>
+      <ResetPassword/>
+      {/* <ForgotPassword/> */}
+      <GetOtpInEmail/>
+      <OtpDesign/>
+      {/* <Sidebar/> */}
+      <DashboardOverview/>
+      <CustomerLists/>
+      <HeroSection/>
+      <Design_Top_Product/>
     </div>
   )
 }
