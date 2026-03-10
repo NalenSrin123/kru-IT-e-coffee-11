82f118747fa01e8aa9a9553cb147a53460bc495a
LuyRayuth <dar95738@gmailcom>
2026-03-10 09:25:52 +0700

    Design_Inventory

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,15 @@
-
-import Login from './utils/Login'
-import Register from './services/auth/register'
-import React from 'react'
-import Design_UI_get_reset_password_in_email from './services/auth/Design_UI_get_reset_password_in_email'
-import ResetPassword from './services/auth/Comfirm_rest_pw'
-import ForgotPassword from './services/auth/Forgotpassword'
-import GetOtpInEmail from './services/auth/GetOtpInEmail'
-import OtpDesign from './services/auth/OtpPage'
-import Sidebar from './dashboard/features/Sidebar/sidebar'
-import { DashboardOverview } from './dashboard/features/dashboard-overview/DashboardMain'
-import CustomerLists from './dashboard/features/a2-tables-filters-ux/confirmation-modals/Customerlists'
-import HeroSection from './components/public/home/HeroSection'
-import Design_Top_Product from './components/public/home/Design_Top_Product'
-import Navbar from './components/Navbar'
+import { useState } from 'react'
+import './App.css'
+import Inventory from './dashboard/features/dashboard-overview/components/Inventory'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      <Login/>
-      <Design_UI_get_reset_password_in_email/>
-      <ResetPassword/>
-      {/* <ForgotPassword/> */}
-      <GetOtpInEmail/>
-      <OtpDesign/>
-      {/* <Sidebar/> */}
-      <DashboardOverview/>
-      <CustomerLists/>
-      <HeroSection/>
-      <Design_Top_Product/>
-      <Register />
-      <Navbar/>
-    </div>
+    <>
+      <Inventory/>
+    </>
   )
 }
 
 export default App
-
