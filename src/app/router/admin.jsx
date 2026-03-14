ee07860fc6f225a8ea07bf6b46a472aa08b446b2
tvy22 <julysok2004@gmail.com>
2026-03-14 13:55:08 +0700

    add inventory to route

--- src/app/router/admin.jsx
+++ src/app/router/admin.jsx
@@ -4,6 +4,7 @@ import CustomerLists from "../../dashboard/features/a2-tables-filters-ux/confirm
 import MenuManagement from "../../dashboard/features/a1-core-crud-forms/crud-menu-coffee";
 import ProtectedRoute from "../../components/ProtectedRoute";
 import DashboardLayout from "../../dashboard/layouts/DashboardLayout";
+import Inventory from "../../dashboard/features/dashboard-overview/components/Inventory";
 
 // const adminRoutes = [
 //   {
@@ -54,6 +55,7 @@ const adminRoutes = [
       { path: "users", element: <UserManagement /> }, 
       { path: "customers", element: <CustomerLists /> }, 
       { path: "menu", element: <MenuManagement /> }, 
+      { path: "inventory", element: <Inventory/> }, 
     ],
   },
 ];
