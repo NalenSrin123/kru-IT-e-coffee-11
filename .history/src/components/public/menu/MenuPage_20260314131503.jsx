46234a81219492e761adec177d66405a4d4f9117
unknown <chhaikoungchrea@gmail.com>
2026-03-19 08:09:11 +0700

    Designed menu page

---
+++ .history/src/components/public/menu/MenuPage_20260314131503.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MenuPage = () => {
+  return (
+    <>
+        <div>
+            <Nav
+        </div>
+    </>
+  )
+}
+
+export default MenuPage
---
+++ .history/src/components/public/menu/MenuPage_20260314132715.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../../Navbar'
+import coffeeBackground2 from "../../../assets/images/coffee/coffeeBackground2";
+import coff
+
+const MenuPage = () => {
+  return (
+    <>
+        <div>
+            <Navbar/>
+            <div className='w-[90%] h-[300px] bg-amber-200 m-auto mt-5 rounded-2xl'>
+                <img src="coffeeBackground2" alt="" />
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default MenuPage
