ac99c2c8db95db4987f2ff10f355fd00120108b5
oudomtray096-star <oudomtray096@gmail.com>
2026-03-19 11:38:47 +0700

    push payment method and payment success

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,29 @@ import PublicLayout from "./app/layouts/PublicLayout";
 import AuthLayout from "./app/layouts/AuthLayout";
 import Login from "./utils/Login";
 
+
 const App = () => {
   return (
     <Routes>
       {/* Public Pages */}
       <Route
-        path="/"
+         path="/"
         element={<PublicLayout />}
-      >
-        <Route index element={<></>} /> {/* Extra content if needed */}
+      > 
+         <Route index element={<></>} /> Extra content if needed 
       </Route>
 
       {/* Auth Pages */}
       <Route
         element={<AuthLayout />}
       >
+      
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Route>
     </Routes>
+    
   );
 };
 
