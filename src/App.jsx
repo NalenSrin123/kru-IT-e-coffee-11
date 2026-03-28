982a1ee201374c887fae059b3dc2ade3a7140fc3
LuyRayuth <dar95738@gmailcom>
2026-03-28 11:11:39 +0700

    Update Inventory component and package files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 
-import React from 'react'
-import Register from './services/auth/register'
+import { useState } from 'react'
+import './App.css'
+import Inventory from './dashboard/features/dashboard-overview/components/Inventory'
+
+
 import Login from './utils/Login'
-import OtpDesign from './services/auth/OtpPage'
+import Register from './services/auth/register'
+import React from 'react'
 import Design_UI_get_reset_password_in_email from './services/auth/Design_UI_get_reset_password_in_email'
 import ResetPassword from './services/auth/Comfirm_rest_pw'
 import ForgotPassword from './services/auth/Forgotpassword'
 import GetOtpInEmail from './services/auth/GetOtpInEmail'
+import OtpDesign from './services/auth/OtpPage'
+import Sidebar from './dashboard/features/Sidebar/sidebar'
+import { DashboardOverview } from './dashboard/features/dashboard-overview/DashboardMain'
+import CustomerLists from './dashboard/features/a2-tables-filters-ux/confirmation-modals/Customerlists'
+import HeroSection from './components/public/home/HeroSection'
+import Design_Top_Product from './components/public/home/Design_Top_Product'
+import Navbar from './components/Navbar'
+import Footer from './components/common/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
 
 import Sidebar from './dashboard/features/Sidebar/sidebar'
 import { DashboardOverview } from './dashboard/features/dashboard-overview/DashboardMain'
@@ -20,6 +35,7 @@ import Footer from './components/common/Footer'
 const App = () => {
   return (
     <div>
+      <Inventory/>
       <Navbar/>
       <HeroSection/>     
       <Design_Top_Product/>
